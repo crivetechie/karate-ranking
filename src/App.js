e7b0501ea912dbc96e1df8bfe3fec7a9d3684263
Marco Crivellaro <marco.crive@gmail.com>
2024-03-12 23:29:00 +0100

    style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./App.css"; // Importing CSS for styling
 
-function NumberInput({ onNumberClick, onEnterClick, onClearNumber, onClearList, currentNumber }) {
+function NumberInput({
+  onNumberClick,
+  onEnterClick,
+  onClearNumber,
+  onClearList,
+  currentNumber,
+}) {
   const [number, setNumber] = useState(0);
 
   const handleNumberClick = (digit) => {
@@ -21,44 +28,78 @@ function NumberInput({ onNumberClick, onEnterClick, onClearNumber, onClearList, 
     setNumber(Math.floor(number / 10));
   };
 
-  const renderNumbers = () => {
-    const buttons = [];
-    for (let i = 1; i <= 9; i++) {
-      buttons.push(
-        <button key={i} onClick={() => handleNumberClick(i)}>
-          {i}
-        </button>
-      );
-    }
-    return buttons;
-  };
-
   return (
-    <div>
-      <div>Current Number: {number / 10}</div>
-      <div>
-        <button onClick={handleClearNumber}>Clear Number</button>
-        <button onClick={onClearList}>Clear List</button>
-        <button onClick={handleBackspaceClick}>Backspace</button>
+    <div className="number-input">
+      <div className="current-number">Current Number: {number / 10}</div>
+      <div className="button-container">
+        <div className="button-row">
+          <button className="digit-button" onClick={() => handleNumberClick(7)}>
+            7
+          </button>
+          <button className="digit-button" onClick={() => handleNumberClick(8)}>
+            8
+          </button>
+          <button className="digit-button" onClick={() => handleNumberClick(9)}>
+            9
+          </button>
+        </div>
+        <div className="button-row">
+          <button className="digit-button" onClick={() => handleNumberClick(4)}>
+            4
+          </button>
+          <button className="digit-button" onClick={() => handleNumberClick(5)}>
+            5
+          </button>
+          <button className="digit-button" onClick={() => handleNumberClick(6)}>
+            6
+          </button>
+        </div>
+        <div className="button-row">
+          <button className="digit-button" onClick={() => handleNumberClick(1)}>
+            1
+          </button>
+          <button className="digit-button" onClick={() => handleNumberClick(2)}>
+            2
+          </button>
+          <button className="digit-button" onClick={() => handleNumberClick(3)}>
+            3
+          </button>
+        </div>
+        <div className="button-row">
+          <button className="digit-button" onClick={handleClearNumber}>
+            Clear
+          </button>
+          <button className="digit-button" onClick={() => handleNumberClick(0)}>
+            0
+          </button>
+          <button className="digit-button" onClick={handleEnterClick}>
+            Enter
+          </button>
+        </div>
+        <div className="button-row">
+          <button className="backspace-button" onClick={handleBackspaceClick}>
+            Backspace
+          </button>
+          <button className="clear-list-button" onClick={onClearList}>
+            Clear List
+          </button>
+        </div>
       </div>
-      <div>
-        {renderNumbers()}
-        <button onClick={() => handleNumberClick(0)}>0</button>
-      </div>
-      <button onClick={handleEnterClick}>Enter</button>
     </div>
   );
 }
 
 function NumberList({ numbers, onSortClick }) {
   return (
-    <div>
+    <div className="number-list">
       <ul>
         {numbers.map((number, index) => (
           <li key={index}>{number / 10}</li>
         ))}
       </ul>
-      <button onClick={onSortClick}>Sort</button>
+      <button className="sort-button" onClick={onSortClick}>
+        Sort
+      </button>
     </div>
   );
 }
@@ -81,19 +122,23 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Number Sorting App</h1>
-      <NumberInput 
-        onNumberClick={(digit) => setCurrentNumber(currentNumber * 10 + digit)}
-        onEnterClick={(number) => {
-          addNumber(number);
-          setCurrentNumber(0);
-        }}
-        onClearNumber={() => setCurrentNumber(0)}
-        onClearList={clearList}
-        currentNumber={currentNumber}
-      />
-      <NumberList numbers={numbers} onSortClick={sortNumbers} />
+      <div className="calculator-container">
+        <NumberInput
+          onNumberClick={(digit) =>
+            setCurrentNumber(currentNumber * 10 + digit)
+          }
+          onEnterClick={(number) => {
+            addNumber(number);
+            setCurrentNumber(0);
+          }}
+          onClearNumber={() => setCurrentNumber(0)}
+          onClearList={clearList}
+          currentNumber={currentNumber}
+        />
+        <NumberList numbers={numbers} onSortClick={sortNumbers} />
+      </div>
     </div>
   );
 }
